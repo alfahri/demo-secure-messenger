e8e6c07749987b91a9788fb491273baff5b79bd1
AL <muhammad.al@satupintu.co.id>
2022-07-26 13:49:40 +0700

    update

---
+++ lib/src/utils/configs.dart
@@ -0,0 +1,6 @@
+
+const String APP_ID = "6412";
+const String AUTH_KEY = "qCD53LTvYu-ZVbf";
+const String AUTH_SECRET = "NM2UauFHpYnHTps";
+const String ACCOUNT_ID = "8395";
+const String DEFAULT_PASS = "12345678";
